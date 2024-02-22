d6f459ebe744bbd72ad18cbcf6d6c83e4e250173
hdlee <dev.fringe@gmail.com>
2024-02-22 16:36:58 +0900

    init

--- src/main/java/org/vaadin/example/view/MainView.java
+++ src/main/java/org/vaadin/example/view/MainView.java
@@ -14,10 +14,8 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 
 import lombok.val;
-import lombok.extern.slf4j.Slf4j;
 
 @Route
-@Slf4j
 public class MainView extends VerticalLayout {
 
     private static final long serialVersionUID = 2857094787956546845L;
@@ -26,9 +24,7 @@ public class MainView extends VerticalLayout {
 	private RestTemplate r;
 
     public MainView(final RestTemplate r) {
-    	log.info("test");
     	this.r = r;
-    	
         t.setPlaceholder("Filter by name...");
         t.setClearButtonVisible(true);
         t.setValueChangeMode(ValueChangeMode.EAGER);
@@ -37,24 +33,19 @@ public class MainView extends VerticalLayout {
         	get();
         });
         val toolbar = new HorizontalLayout(t, b);
-        
 		g.addColumn(Person::getName).setHeader("Name");
 		g.addColumn(o -> Integer.toString(o.getBirth())).setHeader("Year of birth");
 		g.setSelectionMode(SelectionMode.SINGLE);
         val main = new HorizontalLayout(g);
         
         main.setSizeFull();
-    	log.info("test start");
         get();
-    	log.info("test4");
 
         add(toolbar, main);
     }
 
-    @Async void get() {
-    	log.info("test2");
+    void get() {
     	g.setItems(r.getForObject("http://localhost:80/p?text=" + t.getValue(), Person[].class));
-    	log.info("test3");
     }
     
 }
